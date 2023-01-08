5fa3e4bedbc65dbc3951c42756dfafd63fede4df
Santosh <saisantosh.408@gmail.com>
2023-01-08 15:16:54 -0500

    ProfileDropdown and userProfile pages

--- client/src/components/UserProfile.tsx
+++ client/src/components/UserProfile.tsx
@@ -1,13 +1,40 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import noImage from "../assets/noImage.jpg";
+import {useAuth0} from "@auth0/auth0-react"
 
-const events = () => {
+const userProfile = () => {
+    const {user} = useAuth0();
+    console.log(user)
+    
     return (
-        <div className="text-center">
-            <h1 className="text-2xl font-mono leading-7 text-gray-900 sm:text-3xl sm:truncate">User Profile</h1>
-            <Outlet/>
+        <div>
+            <div>
+
+            </div>
+            <div className="rounded-full flex justify-center">
+                <img src={noImage} alt="Profile Photo"/>
+            </div>
+            <div className="flex justify-center">
+                <p className="m-3 font-bold">
+                    First Name
+                </p>
+                <p className="m-3 font-bold">
+                    Last Name
+                </p>
+            </div>
+            <div className="flex justify-center">
+                <p className="font-thin">
+                    Phone
+                </p>
+            </div>
+            <div className="flex justify-center">
+                <p className="font-semibold">
+                    Email
+                </p>
+            </div>
+            <div className="border-2 mx-64 my-10"/>
         </div>
     )
 }
 
-export default events;
+export default userProfile;
